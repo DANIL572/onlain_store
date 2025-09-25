53cec281535ce368d65b0e715f59ffe193097b42
Danil <danilsavrin93@gmail.com>
2025-09-25 19:15:52 +0300

    Прописаны пути к приложениям

--- online_store/online_store/urls.py
+++ online_store/online_store/urls.py
@@ -18,9 +18,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')), # для стилей
+    path('core/', include('core.urls')),
+    path('customers/', include('customers.urls')),
+    path('finance/', include('finance.urls')),
+    path('products/', include('products.urls')),
+    path('reviews/', include('reviews.urls')),
+    path('sellers/', include('sellers.urls')),
 
     # Делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py)
     # подключались к главному приложению с префиксом products/.

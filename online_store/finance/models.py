7625c62ab3ed429034c55a6bcb72f3f478f0df3b
Danil <danilsavrin93@gmail.com>
2025-10-03 17:50:32 +0300

    Добавил модель Account

--- online_store/finance/models.py
+++ online_store/finance/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from core.models import Users
 
-# Create your models here.
+
+
+class Account(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='account')
+    balance = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    class CurrencyChoices(models.TextChoices):
+        RUB = 'RUB', 'Рубль'
+        USD = 'USD', 'Доллар США'
+        EUR = 'EUR', 'Евро'
+    currency = models.CharField(max_length=3, choices=CurrencyChoices.choices, default=CurrencyChoices.RUB)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'account'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.user.name} - {self.balance}{self.currency}'
+
+
+
+
+
+
+
+
+
+
+
